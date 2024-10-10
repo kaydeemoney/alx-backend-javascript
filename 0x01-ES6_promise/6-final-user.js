7fb675c4a5f71feead15dc1a4a1b6a5f9a23cda8
kaydeemoney <daramola4082@gmail.com>
2024-10-10 11:41:49 +0100

    final

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,12 +1,14 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
-export default function handleProfileSignup() {
-  return Promise.allSettled([uploadPhoto('guillaume.jpg'), signUpUser()])
-    .then((res) => (
-      res.map((r) => ({
-        status: r.status,
-        value: r.status === 'fulfilled' ? r.value : String(r.reason),
-      }))
-    ));
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const createPromise = signUpUser(firstName, lastName);
+  const uploadPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([createPromise, uploadPromise]).then((results) => {
+    return results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.error,
+    }));
+  });
 }
